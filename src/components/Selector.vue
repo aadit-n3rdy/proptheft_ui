bedd06e514824a365fafa6a6d9db8821ea5f4e33
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2023-12-07 06:40:22 +0530

    Completed statewise line graph

---
+++ src/components/Selector.vue
@@ -0,0 +1,69 @@
+<template>
+    <span @click="clicked()" :id="$props.id" :class="getClasses()">
+    <slot></slot></span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['modelValue', 'id', 'enabled'])
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        if (props.enabled) {
+            emit('update:modelValue', value)
+        }
+    }
+})
+
+function clicked() {
+    if (props.enabled) {
+        if (value.value === undefined) {
+            value.value = true
+        } else {
+            value.value = !value.value
+        }
+        console.log("clicked")
+    }
+}
+
+function getClasses() {
+    let l = ['selector']
+    if (props.enabled) {
+        l.push('selector-enabled')
+        if (props.modelValue) {
+            l.push('selector-true')
+        } else {
+            l.push('selector-false')
+        }
+    } else {
+        l.push('selector-disabled')
+    }
+    return l
+}
+
+</script>
+
+<style scoped>
+@import url('../assets/base.css');
+.selector {
+    padding: 0.5em;
+}
+.selector-true {
+    background-color: var(--c-dark2);
+    color: var(--c-light1);
+}
+.selector-false {
+    background-color: var(--c-light2);
+    color: var(--c-dark2);
+}
+
+.selector-disabled {
+    background-color: lightgray;
+    color: darkgray;
+}
+</style>
--- src/views/StatewiseLineView.vue
+++ src/views/StatewiseLineView.vue
@@ -1,22 +1,52 @@
 
 <template>
     <PageContainer>
-     <div id="content"> 
-        <div id="text-content">
-            <div id="title">Statewise Line Graph</div>
-            <!-- slider for year adjustment-->
-            <!-- radio buttons for states -->
+        <div id="content">
+            <div id="text-content">
+                <div id="title" class="border-shadow">Statewise Line Graph</div>
+                <!-- slider for year adjustment-->
+                <div id="years-container" class="border-shadow inp-container">
+                    <span class="inp-title">Years</span>
+                    <Slider id="year-inp" :min="1980" :max="2030" :step="10" v-model="yearRange" />
+                </div>
+                <!-- radio buttons for states -->
+                <div id="states-container" class="border-shadow inp-container">
+                    <span class="inp-title">States</span>
+                    <div id="cb-container">
+                        <Selector v-for="state in stateList" v-model="checkedStates[state]" enabled="true"
+                            :id="state + 'inp'">{{ state }}</Selector>
+                    </div>
+                </div>
+            </div>
+            <LineChart id="myChart" class="border-shadow" :datasets="datasets" :labels="labels" />
         </div>
-        <LineChart id="myChart" :datasets="datasets" :labels="labels"/>
-    </div>
     </PageContainer>
 </template>
+
+<script>
+import Slider from '@vueform/slider'
+import Selector from '../components/Selector.vue';
+export default {
+    components: {
+        Slider,
+        Selector
+    },
+}
+</script>
 
 <script setup>
 import LineChart from '../components/LineChart.vue';
 import PageContainer from '../components/PageContainer.vue';
-
 import { ref } from 'vue';
+
+const stateList = ref(
+    ['TN', 'AP', 'DL', 'KL', 'UP', 'MH', 'MP']
+)
+
+const yearRange = ref([1990, 2010])
+
+const checkedStates = ref({})
+
 const datasets = ref(
     [{
         label: '# of Votes',
@@ -24,12 +54,17 @@ const datasets = ref(
         borderWidth: 2,
         borderColor: "#5cddff",
         lineTension: 0,
-      }]
+    }]
 )
 const labels = ref([2015, 2016, 2017, 2018, 2019, 2020])
 </script>
 
 <style scoped>
+.border-shadow {
+    border: 0.1rem solid var(--c-mid);
+    box-shadow: var(--c-mid) 0.1rem 0.1rem;
+}
+
 #content {
     width: 100%;
     height: 95%;
@@ -43,22 +78,77 @@ const labels = ref([2015, 2016, 2017, 2018, 2019, 2020])
     display: grid;
     grid-template-columns: 1fr 2fr;
 }
+
 #text-content {
     display: flex;
     flex-direction: column;
-    border: 1px solid black;
     width: 100%;
     align-items: center;
+    padding: 1em;
+    gap: 2em;
+    height: 100%;
+    justify-content: center;
 }
+
 #title {
     font-weight: bold;
     font-size: 2em;
+    align-content: center;
+    justify-content: center;
+    padding: 1em;
+    width: 100%;
+    background-color: var(--c-light2);
 }
+
 #myChart-wrapper {
-    border: 1px solid black;
     grid-column: 2;
     padding: 2em;
     width: 100%;
     height: 100%;
 }
+
+.inp-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    ;
+    padding: 1.5em;
+    gap: 1em;
+}
+
+.inp-title {
+    font-size: 1.5em;
+    margin: 1em;
+    margin-left: 0;
+    margin-top: 0;
+}
+
+.state-container {
+    display: flex;
+    gap: 0.5em;
+}
+
+#cb-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-items: center;
+    gap: 0.5em;
+}
+
+#year-inp {
+    --slider-connect-bg: var(--c-mid);
+    --slider-tooltip-bg: var(--c-mid);
+    --slider-tooltip-font-size: 0.8em;
+    --slider-tooltip-line-height: 1.5em;
+    transition-duration: 0;
+}
+
+#year-inp * {
+    transition-duration: 0;
+    ;
+}
 </style>
+
+<style src="@vueform/slider/themes/default.css"></style>
